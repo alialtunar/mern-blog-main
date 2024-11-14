654c8cab00305651ec6b779eb7fc8137ae79e240
alialtunar <altunarali284@gmail.com>
2024-11-14 22:18:11 +0300

    AAA

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use(cors({ origin: ['http://localhost:5137', 'http://localhost:5173','https:
 
 
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000!');
 });
 
